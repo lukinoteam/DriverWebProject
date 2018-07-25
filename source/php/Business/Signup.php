04e9530de37860d842f348f5d1cbde124959733d
xwind1999 <40222758+xwind1999@users.noreply.github.com>
2018-07-25 09:09:50 +0700

    upload php files

--- source/php/Business/Signup.php
+++ source/php/Business/Signup.php
@@ -1,57 +1,62 @@
 <?php
-require_once 'Connectdb.php';
-//MARK:- Variables
+require_once __DIR__ ."/../DataAccess/MySQL/MySQLDA.php";
 $name = $_POST['fieldName'];
 $email = $_POST['fieldEmail'];
 $pass = $_POST['fieldPass'];
 $gender = $_POST['fieldGender'];
 $genderInt = 0;
-
-//MARK:- Operation
-
-//TO-DO: check Is unique email ??
+//check Is unique email ??
 $isUnique = 1;
-if($GLOBALS['gender'] == "MALE"){
-	$GLOBALS['genderInt'] = 1;
+if($GLOBALS['gender']=="MALE"){
+    $GLOBALS['genderInt'] = 1;
 }
-else if($GLOBALS['gender'] == "FEMALE"){
-	$GLOBALS['genderInt']  = 2;
+else if($GLOBALS['gender']=="FEMALE"){
+    $GLOBALS['genderInt']  = 2;
 }
 else {
-	$GLOBALS['genderInt']  = 3;
+    $GLOBALS['genderInt']  = 3;
 }
-//@var: User's properties
 $dob = $_POST['fieldDate'];
-$id = time();
+$id = "";
 $md5Pass = md5($pass);
-$db = MySQLConnectivity::get_instance();
-$conn = $db->get_connection();
-
-//MARK: Check connection
-
-//@var: SQL 
-$selectSQL = "SELECT email FROM users";
-$insertSQL = "INSERT INTO users (id, email, fullname, password, dob, gender) VALUES ('$id','$email', '$name', '$md5Pass', '$dob', '$genderInt')";   
-//TO-DO: take a list of email from DB
-$result = $conn->query($selectSQL);
-if ($result->num_rows > 0) {
-    //TO-DO: output data of each row
-    while($row = $result->fetch_assoc()) {
-        if($row["email"]==$GLOBALS['email']){
-            $GLOBALS['isUnique'] = 0;
+//Create Connection   
+//define table name , attribute to select
+//take unique id
+$table = 'users';
+$attribute = 'email';
+$query = new MySQLDA();
+$GLOBALS['id'] = $query->takeUUID($table);
+//selectQuery = Select email from users;
+$selectQuery=$query->select($table,$attribute);
+    if ($selectQuery->num_rows > 0) {
+    // output data of each row
+        while($row = $selectQuery->fetch_assoc()) {
+            if($row["email"]==$GLOBALS['email']){
+                $GLOBALS['isUnique'] = 0;
+            }
         }
     }
-}
-//TO-DO: if the email is unique
-if($GLOBALS['isUnique'] == 1) {   
-    if($conn->query($insertSQL) == TRUE) {
+//$insert = "INSERT INTO users (id,email,fullname,password,dob,gender) VALUES ('$id','$email','$name','$md5Pass','$dob','$genderInt')";
+$arrayData = array(
+"id" => $id,
+"email" => $email,
+"fullname" => $name,
+"password" => $md5Pass,
+"dob" => $dob,
+"gender" => $genderInt,
+);
+$insert = $query->insert($table,$arrayData);   
+//if the email is unique we insert data
+if($GLOBALS['isUnique']==1){   
+    if($GLOBALS['insert']){
         echo "true";
     }
-    else {
-        echo "false";
+    else{
+        echo "fasle";
     }            
 }
-else {
-        echo "Account has already existed";
+else{
+    echo "Account has already existed";
 }
-?>
+    
+  ?>
