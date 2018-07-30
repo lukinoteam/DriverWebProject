1b547c9e340ae4c0e7d37a784d497135a1e82ed6
dungdl28d11 <33054551+dungdl28d11@users.noreply.github.com>
2018-07-30 11:51:41 +0700

    Adjust Elastic DataAccess for deletes

--- source/php/DataAccess/ElasticDataAccess/ElasticDA.php
+++ source/php/DataAccess/ElasticDataAccess/ElasticDA.php
@@ -16,7 +16,7 @@ final class ElasticDA
         $this->helper = new FileHelper();
     }
 
-//MARK:- Index Function
+//MARK:- Index Functions
 
     //TO-DO: Indexing file's info at @var($file_input) into Elastic database
     public function indexing($file_input, $file_path)
@@ -46,6 +46,26 @@ final class ElasticDA
             return null;
         }
     }
+
+     //TO-DO: Indexing file's info at @var($folder_id) into Elastic database
+     public function index_folder($folder_id, $status)
+     {
+         $params = [
+             'index' => 'folder',
+             'type' => 'info',
+             'id' => $folder_id,
+             'body' => [
+                 'status' => $status
+             ],
+         ];
+         try {
+             $response = $this->client->index($params);
+             return "Success";
+         } catch (Elasticsearch\Common\Exceptions $e) {
+             echo $e;
+             return null;
+         }
+     }
 //MARK:- Search Functions
     //TO-DO: Search with @var($file_name as String) in Elastic database
     public function search_with_name($file_name)
@@ -240,12 +260,65 @@ final class ElasticDA
             return null;
         }
     }
+    //TO-DO: Search folder with @var($status as Integer) in Elastic database
+    public function search_folder_with_status($status)
+    {
+        $params = [
+            'index' => 'folder',
+            'type' => 'info',
+            'size' => 100,
+            'body' => [
+                'query' => [
+                    'bool' => [
+                        'should' => [
+                            ['match' => ['status' => $status]],
+                        ],
+                    ],
+                ],
+            ],
+        ];
+        try {
+            $response = $this->client->search($params);
+            $resource = array();
+            for ($i = 0; $i < count($response['hits']['hits']); $i++) {
+                $file_id = strtoupper(json_encode($response['hits']['hits'][$i]['_id']));
+                array_push($resource, $file_id);
+            }
+            return $resource;
+        } catch (Elasticsearch\Common\Exceptions $e) {
+            echo $e;
+            return null;
+        }
+    }
 //MARK:- Update Function
+    //TO-DO: Update file with $id and $status
     public function update_with_status($id, $status)
     {
         $params = [
             'index' => 'file',
             'type' => 'content',
+            'id' => $id,
+            'body' => [
+                'doc' => [
+                    'status' => $status
+                ]
+            ],
+        ];
+        try {
+            $response = $this->client->update($params);
+            return $response;
+        } catch (Elasticsearch\Common\Exceptions $e) {
+            echo $e;
+            return null;
+        }
+    }
+
+    //TO-DO: Update folder with $id and $status
+    public function update_folder_with_status($id, $status)
+    {
+        $params = [
+            'index' => 'folder',
+            'type' => 'info',
             'id' => $id,
             'body' => [
                 'doc' => [
