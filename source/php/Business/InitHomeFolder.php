4ee6956645e4489a746a5b1d1aa31107c2a1fce8
lukinoteam <nguyenviethung_t61@hus.edu.vn>
2018-07-31 20:20:45 +0700

    Remove recycle bin initialize

--- source/php/Business/InitHomeFolder.php
+++ source/php/Business/InitHomeFolder.php
@@ -9,16 +9,6 @@ function init_home_folder($id)
             VALUES (" . $user_id . ",415fe87d-9b3b-4a4c-8f64-b380d2b39240,'HOME',0,dateof(now()),'DEFAULT', 0);",
         "INSERT INTO folder (folder_id, file_id)
             VALUES (415fe87d-9b3b-4a4c-8f64-b380d2b39240, 415fe87d-9b3b-4a4c-8f64-b380d2b39240)",
-
-        "INSERT INTO folder_info (user_id, folder_id, name, size, date_modify, description, status)
-            VALUES (" . $user_id . ",d2cea1a3-b55e-49ae-956f-1ea7ba9b33a1,'Recycle Bin',0,dateof(now()),'DEFAULT', 0)",
-
-        "INSERT INTO folder (folder_id, file_id)
-            VALUES (d2cea1a3-b55e-49ae-956f-1ea7ba9b33a1, d2cea1a3-b55e-49ae-956f-1ea7ba9b33a1)",
-
-        "INSERT INTO folder (folder_id, file_id)
-            VALUES (415fe87d-9b3b-4a4c-8f64-b380d2b39240, d2cea1a3-b55e-49ae-956f-1ea7ba9b33a1)",
-
     );
 
     foreach ($statement_array as $str){
