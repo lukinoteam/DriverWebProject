557aa6617a7e5d20eec02726c7344e0750a249ac
lukinoteam <nguyenviethung_t61@hus.edu.vn>
2018-08-05 20:23:23 +0700

    Search feature

--- source/php/Business/Search.php
+++ source/php/Business/Search.php
@@ -1,15 +1,12 @@
 <?php
-require_once __DIR__ ."/../DataAccess/ElasticDataAccess/ElasticDA.php";
+require_once __DIR__ . "/../DataAccess/ElasticDataAccess/ElasticDA.php";
+require_once __DIR__ . "/../DataAccess/Cassandra/CassandraDA.php";
 
-
-//start session to get user-id
-session_start();
-$user_id = $_SESSION['id'];
-
-//init connection to elasticsearch
 $eDA = new ElasticDA;
 $search_content = $_POST['key'];
 $search_type = $_POST['type'];
+session_start();
+$user_id = $_SESSION['id'];
 
 $search_result;
 
@@ -25,6 +22,41 @@ if ($search_type === "Name") {
     $search_result = $eDA->search_with_content($search_content, $user_id);
 }
 
-echo json_encode($search_result);
 
-?>
+// init connect to cassandra
+$connect = new CassandraDA();
+
+$result_data = array();
+
+foreach ($search_result as $single) {
+    $single = str_replace('"', '', $single);
+    $statement = new Cassandra\SimpleStatement(
+        "select * from file_info where user_id = " . $user_id . " and file_id = " . $single
+    );
+    $tmpResult = $connect->get_connection()->execute($statement);
+    $thumb = "";
+    if (($tmpResult[0]['type'] == 7 || $tmpResult[0]['type'] == 8 || $tmpResult[0]['type'] == 9) && $tmpResult[0]['size'] < 20 * 1024 * 1024) {
+
+        $statement = new Cassandra\SimpleStatement(
+            "select blobAsAscii(image) as image from thumbnail where file_id = " . $single
+        );
+        $thumbBlob = $connect->get_connection()->execute($statement);
+        $thumb = "data:image/jpg;base64," . base64_encode(pack("H*", $thumbBlob[0]['image']));
+
+    } else {
+        $statement = new Cassandra\SimpleStatement(
+            "select type, blobAsAscii(image) as image from thumbnail where file_id = 011643db-8195-45e7-808c-dddc23461fdb"
+        );
+        $thumbBlob = $connect->get_connection()->execute($statement);
+
+        foreach ($thumbBlob as $res) {
+            if ($tmpResult[0]['type'] == $res['type']) {
+                $thumb = "data:image/jpg;base64," . base64_encode(pack("H*", $res['image']));
+            }
+        }
+    }
+
+    array_push($result_data, array($tmpResult[0]['file_id'], $tmpResult[0]['date_modify'], $tmpResult[0]['name'], $tmpResult[0]['size'], $tmpResult[0]['description'], $thumb, $tmpResult[0]['type'], $tmpResult[0]['status']));
+}
+
+echo json_encode($result_data);
