1347ecace75e8039332ca3ad978681ded4455985
xwind1999 <40222758+xwind1999@users.noreply.github.com>
2018-08-23 10:49:35 +0700

    Update file_handle_function.js

--- source/script/file_handle_function.js
+++ source/script/file_handle_function.js
@@ -196,12 +196,30 @@ function uploadFile() {
             contentType: false,
             processData: false,
             data: form_data,
+            dataType: 'json',
             success: function(data) {
-                specialFolderAction("home");
-                getFileList();
+                
                 $("#inputFile").val("");
                 $('#txtFileDesc').val("");
                 $("#txtFileName").val("");
+                var str = '<li>\
+                <div id="' + data[0].uuid + '" class="fileItem" onclick="triggerFileChoosedTools(this.id);" ondblclick="viewImg(this.id);">\
+                    <div class="img-wrapper">\
+                        <img src=' + data[5] + '>\
+                    </div>\
+                    <div class="fileCaption">\
+                        <p class="name">' + data[2] + '</p>\
+                        <p class="size" style="display: none;">' + parseSize(data[3].value) + '</p>\
+                        <p class="date" style="display: none;">' + parseDate(new Date(data[1].seconds * 1000)) + '</p>\
+                        <p class="type" style="display: none;">' + extIntToStr(data[6]) + '</p>\
+                        <p class="desc" style="display: none;">' + data[4] + '</p>\
+                    </div>\
+                </div>\
+            </li>';
+
+                    $("#fileList").append(str);
+            $("#snackbar").css("visibility", "hidden");
+
             }
         });
     }
