39ae673f5b5acb42549fc0b88e7efe29e2b351f5
lukinoteam <nguyenviethung_t61@hus.edu.vn>
2018-08-23 14:00:31 +0700

    Remove fixed home folder id

--- source/php/Business/InitHomeFolder.php
+++ source/php/Business/InitHomeFolder.php
@@ -6,9 +6,9 @@ function init_home_folder($id)
     $user_id = new Cassandra\UUID($id);
     $statement_array = array(
         "INSERT INTO folder_info (user_id, folder_id, name, date_modify, description, status)
-            VALUES (" . $user_id . ",415fe87d-9b3b-4a4c-8f64-b380d2b39240,'HOME',dateof(now()),'DEFAULT', 0);",
+            VALUES (" . $user_id . "," . $user_id . ",'HOME',dateof(now()),'DEFAULT', 0);",
         "INSERT INTO folder (folder_id, file_id,status)
-            VALUES (415fe87d-9b3b-4a4c-8f64-b380d2b39240, 415fe87d-9b3b-4a4c-8f64-b380d2b39240,1)",
+            VALUES (" . $user_id . ", " . $user_id . ", 1)",
     );
 
     foreach ($statement_array as $str) {
