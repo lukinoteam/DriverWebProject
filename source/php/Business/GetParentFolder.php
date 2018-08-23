e18476d8b5904f6e1320b8d8be7569dc881711ba
lukinoteam <nguyenviethung_t61@hus.edu.vn>
2018-08-23 14:19:34 +0700

    Update GetParentFolder.php

--- source/php/Business/GetParentFolder.php
+++ source/php/Business/GetParentFolder.php
@@ -3,6 +3,9 @@ require_once __DIR__ ."/../DataAccess/Cassandra/CassandraDA.php";
 
 $connect = new CassandraDA();
 
+session_start();
+$user_id = $_SESSION['id'];
+
 // GET CURRENT FOLDER FROM CLIENT
 $current = $_POST['current'];
 
@@ -17,7 +20,18 @@ $result = $connect->get_connection()->execute($statement);
 // PUSH PARENT FOLDER TO CLIENT
 foreach($result as $row){
     if ($row['folder_id'] != $row['file_id']){
-        array_push($data, $row['folder_id']);
+
+        $statement = new Cassandra\SimpleStatement(
+            "select * from folder_info where user_id = ".$user_id." and folder_id = ".$row['folder_id']
+        );
+        $folderInfo = $connect->get_connection()->execute($statement);
+
+        if ($folderInfo[0]['name'] == "HOME"){
+            array_push($data, "home");
+        }
+        else{
+            array_push($data, $row['folder_id']);
+        }
     }
 }
 
